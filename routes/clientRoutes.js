1e0deb5da970a3d14c5419e6625997c5c088b6d7
Andre Lobo <loboandre@hotmail.com>
2024-06-03 14:01:16 -0400

    fix: clientRoutes

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -2,20 +2,20 @@
 const express = require('express');
 const router = express.Router();
 const ClientController = require('../controllers/clientController');
-const authenticateToken = require('../middlewares/authenticateToken');
+// const authenticateToken = require('../middlewares/authenticateToken');
 
 // Rota para criar um novo cliente
-router.post('/',authenticateToken , ClientController.createClient);
+router.post('/', ClientController.createClient);
 
 
 // Rota para obter detalhes de um usuário pelo ID
-router.get('/:id',authenticateToken, ClientController.getClientById);
+router.get('/:id', ClientController.getClientById);
 
 // Rota para atualizar os detalhes de um usuário pelo ID
-router.put('/:id', authenticateToken, ClientController.updateClientById);
+router.put('/:id', ClientController.updateClientById);
 
 // Rota para excluir um usuário pelo ID
-router.delete('/:id', authenticateToken , ClientController.deleteClientById);
+router.delete('/:id',  ClientController.deleteClientById);
 
 // Rota para listar todos os usuários
 router.get('/', ClientController.getAllClients);
